dbfa83eadf7055693172d6544d1b93b487e7fc7a
erkabb <erkabb816@gmail.com>
2024-08-07 12:30:56 +0800

    f

--- src/pages/blog/[id].jsx
+++ src/pages/blog/[id].jsx
@@ -1,21 +1,23 @@
-// import React, {useEffect, useState} from "react";
-// import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 const { useEffect } = require("react");
 
-// const ArticleDetail = () => {
-//   const query = useRouter();
-//   console.log("qr", query.query, id);
-// const [articleDetail, setArticleDetail] = useState(null);
+const ArticleDetail = () => {
+  const query = useRouter();
+  console.log("qr", query.query, id);
+  const [articleDetail, setArticleDetail] = useState(null);
 
-//   const getArticlebyId = async (id) => {
-//     const res =await fetch(`link${id}` + id);
-//     const data =await res.json();
-// setArticleDetail(data);
-//   };
-// useEffect(() => {
-//   getArticlebyId(query.id);
-// }, []);
-//   return <div dangerouslySetInnerHtml={{__html: articleDetail?.body_html}}></div>;
-// };
-// export default ArticleDetail;
+  const getArticlebyId = async (id) => {
+    const res = await fetch(`link${id}` + id);
+    const data = await res.json();
+    setArticleDetail(data);
+  };
+  useEffect(() => {
+    getArticlebyId(query.id);
+  }, []);
+  return (
+    <div dangerouslySetInnerHtml={{ __html: articleDetail?.body_html }}></div>
+  );
+};
+export default ArticleDetail;
