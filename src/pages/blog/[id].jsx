7f31f60021c6d0d22dd9caee56a9ad46ff1c1bfe
erkabb <erkabb816@gmail.com>
2024-08-07 12:20:26 +0800

    f

---
+++ src/pages/blog/[id].jsx
@@ -0,0 +1,21 @@
+// import React, {useEffect, useState} from "react";
+// import { useRouter } from "next/router";
+
+const { useEffect } = require("react");
+
+// const ArticleDetail = () => {
+//   const query = useRouter();
+//   console.log("qr", query.query, id);
+// const [articleDetail, setArticleDetail] = useState(null);
+
+//   const getArticlebyId = async (id) => {
+//     const res =await fetch(`link${id}` + id);
+//     const data =await res.json();
+// setArticleDetail(data);
+//   };
+// useEffect(() => {
+//   getArticlebyId(query.id);
+// }, []);
+//   return <div dangerouslySetInnerHtml={{__html: articleDetail?.body_html}}></div>;
+// };
+// export default ArticleDetail;
